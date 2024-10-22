e5ba806cc900f3ef806da53231781218dd034b4e
Pavel Feldman <feldmanpk@gmail.com>
2024-10-22 18:08:16 +0300

    CW #62 Logging functionality

---
+++ src/LoggerAppl.java
@@ -0,0 +1,29 @@
+import java.util.logging.*;
+
+public class LoggerAppl {
+	static Logger logger = Logger.getLogger("LoggerAppl");
+	
+	public static void main(String[] args) throws Exception {
+		LogManager.getLogManager().reset(); // сброс логгера по умолчанию
+		
+		logger.setLevel(Level.FINE); //дб переменная среды
+		Handler handler = new ConsoleHandler();
+		Handler handlerFile = new FileHandler("logs");
+		handler.setLevel(Level.FINEST);
+		handlerFile.setLevel(Level.FINEST);
+		handlerFile.setFormatter(new SimpleFormatter());
+		
+		logger.addHandler(handler);
+		logger.addHandler(handlerFile);
+		
+		logger.severe("severe log");
+		logger.warning("warning log");
+		logger.info("info log");
+		logger.config("config log");
+		logger.fine("fine log");
+		logger.finer("finer log");
+		logger.finest("finest log");
+
+	}
+
+}
